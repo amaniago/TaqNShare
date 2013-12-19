2be9730212f906210a5949e70d22f19d91c32b0a
amaniago <anthony.maniago@viacesi.fr>
2013-12-19 15:04:25 +0100

    -Implémentation des fonctions de sélections et de prise de photo. -Stockage des paramètres dans les applications settings

---
+++ TaqNShare/Data/Parametre.cs
@@ -0,0 +1,14 @@
+﻿namespace TaqNShare.Data
+{
+    public class Parametre
+    {
+        public int Id { get; set; }
+        public int Valeur { get; set; }
+
+        public Parametre(int id, int valeur)
+        {
+            this.Id = id;
+            this.Valeur = valeur;
+        }
+    }
+}
---
+++ TaqNShare/Views/ValidationPhotoPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace TaqNShare.Views
+{
+    public partial class ValidationPhotoPage : PhoneApplicationPage
+    {
+        public ValidationPhotoPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            BitmapImage imageSelectionne = (BitmapImage)PhoneApplicationService.Current.State["image"];
+            ImageSelectionne.Source = imageSelectionne;
+            base.OnNavigatedTo(e);
+        }
+
+        protected override void OnBackKeyPress(System.ComponentModel.CancelEventArgs e)
+        {
+            PhoneApplicationService.Current.State.Clear();
+            base.OnBackKeyPress(e);
+        }
+
+        private void BoutonAccueil_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            PhoneApplicationService.Current.State.Clear();
+            NavigationService.GoBack();
+        }
+    }
+}
