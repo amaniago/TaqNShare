155e40d2acc88b8549bae82d02ca74d9b96b2d41
amaniago <anthony.maniago@viacesi.fr>
2014-02-28 10:16:47 +0100

    -Ajout des fonctions pour la gestion des défis

---
+++ ServeurTaqnshare/ClasseDeService/DefiService.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace ServeurTaqnshare.ClasseDeService
+{
+    /// <summary>
+    /// Classe permettant de retourner un defi, son image et sa composition à travers le web service
+    /// </summary>
+    [DataContract(IsReference = true)]
+    public sealed class DefiService
+    {
+        [DataMember]
+        public int IdDefi { get; set; }
+        [DataMember]
+        public byte[] ImageDefi { get; set; }
+        [DataMember]
+        public string IdUtilisateur { get; set; }
+        [DataMember]
+        public string IdAdversaireDefi { get; set; }
+        [DataMember]
+        public string NomDefi { get; set; }
+        [DataMember]
+        public int ScoreUtilisateurDefi { get; set; }
+        [DataMember]
+        public List<Composition> Composition { get; set; }
+        [DataMember]
+        public string NomUtilisateur { get; set; }
+        [DataMember]
+        public string PrenomUtilisateur { get; set; }
+
+        public DefiService(Defi defi, byte[] imageDefi)
+        {
+            IdDefi = defi.id_defi;
+            IdUtilisateur = defi.id_utilisateur;
+            IdAdversaireDefi = defi.id_adversaire_defi;
+            NomDefi = defi.nom_defi;
+            ScoreUtilisateurDefi = Convert.ToInt32(defi.score_utilisateur_defi);
+            ImageDefi = imageDefi;
+
+            Composition = new List<Composition>();
+
+            foreach (var composer in defi.Composers)
+            {
+                Composition.Add(new Composition(composer));
+            }
+        }
+
+        public DefiService(Defi defi, Utilisateur createurDefi)
+        {
+            IdDefi = defi.id_defi;
+            NomDefi = defi.nom_defi;
+            NomUtilisateur = createurDefi.nom_utilisateur;
+            PrenomUtilisateur = createurDefi.prenom_utilisateur;
+        }
+
+    }
+
+    [DataContract(IsReference = true)]
+    public class Composition
+    {
+        public Composition(Composer composer)
+        {
+            IdPiece = composer.id_piece;
+            IdFiltre = composer.id_filtre;
+            IndexPosition = Convert.ToInt32(composer.position_piece);
+        }
+
+        [DataMember]
+        public int IdPiece { get; set; }
+        [DataMember]
+        public int IdFiltre { get; set; }
+        [DataMember]
+        public int IndexPosition { get; set; }
+    }
+}
--- ServeurTaqnshare/IServiceTaqnshare.cs
+++ ServeurTaqnshare/IServiceTaqnshare.cs
@@ -1,99 +1,86 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations;
 using System.IO;
 using System.Linq;
 using System.ServiceModel;
+using ServeurTaqnshare.ClasseDeService;
 
 namespace ServeurTaqnshare
 {
+    /// <summary>
+    /// Interface permettant d'exposer les méthodes du web service
+    /// </summary>
     [ServiceContract(Name = "ServiceTaqnshare")]
     public interface IServiceTaqnshare
     {
         [OperationContract]
-        string EnvoyerImage(byte[] imageByte, string nomImage);
+        string EnregistrerScore(Utilisateur utilisateurCourant, int scorePartie);
 
         [OperationContract]
-        byte[] RecupererImage(string nomImage);
+        string CreerDefi(Defi partieUtilisateur, List<Composer> compositionTaquin, byte[] imageDefi);
 
         [OperationContract]
-        bool EnregistrerScore(Utilisateur utilisateurCourant, int scorePartie);
+        DefiService RecupererDefi(int idDefi);
 
         [OperationContract]
-        string CreerDefi(Defi partieUtilisateur, List<Composer> compositionTaquin);
+        string ModifierDefi(Defi defiTermine);
+
+        [OperationContract]
+        List<DefiService> RecupererDefis(string idUtilisateur);
     }
 
     public class ServiceTaqnshare : IServiceTaqnshare
     {
-        public string EnvoyerImage(byte[] imageByte, string nomImage)
-        {
-            try
-            {
-                MemoryStream ms = new MemoryStream(imageByte);
-                //FileStream fs = new FileStream(System.Web.Hosting.HostingEnvironment.MapPath("~/TransientStorage/") + fileName, FileMode.Create);
-                FileStream fs = new FileStream(Directory.GetCurrentDirectory() + "\\ImagesWebService\\" + nomImage, FileMode.Create);
-
-                ms.WriteTo(fs);
-                ms.Close();
-                fs.Close();
-                fs.Dispose();
-                return "OK";
-            }
-            catch (Exception ex)
-            {
-                return (ex.ToString());
-            }
-        }
-
-        public byte[] RecupererImage(string nomImage)
-        {
-
-            //return File.ReadAllBytes(System.Web.Hosting.HostingEnvironment.MapPath("~/TransientStorage/") + fileName);
-            return File.ReadAllBytes(Directory.GetCurrentDirectory() + "\\ImagesWebService\\" + nomImage);
-
-        }
-
         /// <summary>
         /// Méthode permettant d'enregistrer le score de l'utilisateur
         /// </summary>
         /// <param name="utilisateurCourant"></param>
         /// <param name="scorePartie"></param>
         /// <returns></returns>
-        public bool EnregistrerScore(Utilisateur utilisateurCourant, int scorePartie)
+        public string EnregistrerScore(Utilisateur utilisateurCourant, int scorePartie)
         {
             try
             {
-                using (var db = new TaqnshareEntities())
-                {
-                    var utilisateur = (from u in db.Utilisateurs
-                                       where u.id_utilisateur == utilisateurCourant.id_utilisateur
-                                       select u).SingleOrDefault();
+                var db = new TaqnshareEntities();
 
-                    if (utilisateur != null)
-                    {
-                        utilisateur.nombre_partie_utilisateur += 1;
-                        utilisateur.score_total_utilisateur += scorePartie;
-                    }
-                    else
-                    {
-                        Utilisateur nouvelUtilisateur = new Utilisateur
-                                                        {
-                                                            id_utilisateur = utilisateurCourant.id_utilisateur,
-                                                            nombre_partie_utilisateur = 1,
-                                                            score_total_utilisateur = scorePartie
-                                                        };
-                        db.Utilisateurs.Add(nouvelUtilisateur);
-                    }
-                    db.SaveChanges();
+                var utilisateur = (from u in db.Utilisateurs
+                                   where u.id_utilisateur == utilisateurCourant.id_utilisateur
+                                   select u).SingleOrDefault();
+
+                if (utilisateur != null)
+                {
+                    utilisateur.nombre_partie_utilisateur += 1;
+                    utilisateur.score_total_utilisateur += scorePartie;
                 }
-                return true;
+                else
+                {
+                    Utilisateur nouvelUtilisateur = new Utilisateur
+                                                    {
+                                                        id_utilisateur = utilisateurCourant.id_utilisateur,
+                                                        nombre_partie_utilisateur = 1,
+                                                        score_total_utilisateur = scorePartie
+                                                    };
+                    db.Utilisateurs.Add(nouvelUtilisateur);
+                }
+                db.SaveChanges();
+
+                return "OK";
             }
             catch (Exception e)
             {
-                return false;
+                return e.Message;
             }
         }
 
-        public string CreerDefi(Defi partieUtilisateur, List<Composer> compositionTaquin)
+        /// <summary>
+        /// Méthode permettant de créer un défi et de stocker l'image associer
+        /// </summary>
+        /// <param name="partieUtilisateur"></param>
+        /// <param name="compositionTaquin"></param>
+        /// <param name="imageDefi"></param>
+        /// <returns></returns>
+        public string CreerDefi(Defi partieUtilisateur, List<Composer> compositionTaquin, byte[] imageDefi)
         {
             try
             {
@@ -112,13 +99,97 @@ namespace ServeurTaqnshare
                 db.Composers.AddRange(compositionTaquin);
                 db.SaveChanges();
 
+                MemoryStream ms = new MemoryStream(imageDefi);
+                FileStream fs = new FileStream(Directory.GetCurrentDirectory() + "\\ImagesWebService\\Image" + nouveauDefi.id_defi + ".jpg", FileMode.Create);
+
+                ms.WriteTo(fs);
+                ms.Close();
+                fs.Close();
+                fs.Dispose();
+
                 return "OK";
             }
             catch (Exception e)
             {
-                return e.Message;
+                return e.InnerException.ToString();
             }
         }
+
+        /// <summary>
+        /// Méthode permettant de récupérer un défi et son image
+        /// </summary>
+        /// <param name="idDefi"></param>
+        /// <returns></returns>
+        public DefiService RecupererDefi(int idDefi)
+        {
+            var db = new TaqnshareEntities();
+
+            var defi = (from defis in db.Defis
+                        where defis.id_defi == idDefi
+                        select defis).Single();
+
+            byte[] imageDefi = File.ReadAllBytes(Directory.GetCurrentDirectory() + "\\ImagesWebService\\Image" + idDefi + ".jpg");
+
+            DefiService retour = new DefiService(defi, imageDefi);
+
+            return retour;
+        }
+
+        /// <summary>
+        /// Méthode permettant de modifier un défi lorsque celui-ci a été accepter par l'adversaire
+        /// </summary>
+        /// <param name="defiTermine"></param>
+        /// <returns></returns>
+        public string ModifierDefi(Defi defiTermine)
+        {
+            try
+            {
+                var db = new TaqnshareEntities();
+
+                var defi = (from defis in db.Defis
+                            where defis.id_defi == defiTermine.id_defi
+                            select defis).Single();
+
+                defi.resolu = defiTermine.resolu;
+                defi.score_adversaire_defi = defiTermine.score_adversaire_defi;
+
+                db.Defis.AddOrUpdate(defi);
+                db.SaveChanges();
+
+                return "OK";
+            }
+            catch (Exception e)
+            {
+                return e.InnerException.ToString();
+            }
+        }
+
+        public List<DefiService> RecupererDefis(string idUtilisateur)
+        {
+
+            var db = new TaqnshareEntities();
+
+            List<Defi> defis = (from d in db.Defis
+                                where d.id_adversaire_defi == idUtilisateur
+                                select d).ToList();
+
+            
+
+            List<DefiService> defisRetour = new List<DefiService>();
+            foreach (var defi in defis)
+            {
+                var utilisateur = (from u in db.Utilisateurs
+                                   where u.id_utilisateur == defi.id_utilisateur
+                                   select u).SingleOrDefault();
+
+                defisRetour.Add(new DefiService(defi, utilisateur));
+            }
+
+            return defisRetour;
+
+        }
     }
+
+
 }
 
